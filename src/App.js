924e5e033da519576fc0520e058cd8beee81ae24
PhillipJeffries <ggbox@mail.ru>
2021-09-24 14:25:27 +0400

    hypno

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
-import './App.css';
+import Logo2 from './images/153863';
+import logo3 from './images/154466.svg';
+import logo4 from './images/304485.svg';
+//import logo5 from './images/1294347.svg';
+import logo6 from './images/1991340.svg';
+import logo7 from './images/2790215.svg';
+import './App.scss';
+
+import Logo from './images/1294347'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="App-logo-wrapper">
+        <Logo clas="App-logo12"/>
+        <Logo clas="App-logo11"/>
+        <Logo clas="App-logo10"/>
+        <Logo clas="App-logo9"/>
+        <Logo clas="App-logo8"/>
+        <Logo clas="App-logo7"/>
+        <Logo clas="App-logo6"/>
+        <Logo clas="App-logo5"/>
+        <Logo clas="App-logo4"/>
+        <Logo clas="App-logo3"/>
+        <Logo clas="App-logo2"/>
+        <Logo clas="App-logo1"/>
+        <Logo clas="App-logo"/>
+    
+
+      </div>
+
+        
       </header>
     </div>
   );
